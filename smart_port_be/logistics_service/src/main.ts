5f4680b03f6e42fab1a2bee6f13ff36a4fa31503
Akila Piumantha <156569575+Akila-Piyumantha@users.noreply.github.com>
2026-04-26 15:45:27 +0530

    Add Kafka microservice and vessel message handlers

--- smart_port_be/logistics_service/src/main.ts
+++ smart_port_be/logistics_service/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Connect Kafka as a Microservice
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: [process.env.KAFKA_BROKERS || 'localhost:9092'],
+      },
+      consumer: {
+        groupId: 'logistics-consumer-server', // A unique group for logistics consumption
+      },
+    },
+  });
+
+  await app.startAllMicroservices(); // Start Kafka listening
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
